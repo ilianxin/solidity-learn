46773812a445e8bce89d68e3e671fe9889910cfa
ilianxin <ilianxin@126.com>
2025-08-09 13:46:12 +0800

    ini

---
+++ Voting.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Voting {
+    mapping (string => uint256) private voteMap; // Mapping from candidate names to vote counts
+
+    string[] private volunteers; 
+
+    constructor ()  {
+        voteMap["Alice"] = 0;
+        voteMap["Bob"] = 0;
+        voteMap["Charlie"] = 0;
+        voteMap["Cool"] = 0;
+
+        volunteers.push("Alice");
+        
+        volunteers.push("Bob");
+        
+        volunteers.push("Charlie");
+        
+        volunteers.push("Cool");
+
+    }
+
+    function vote(string memory name, uint256 number) public {
+        voteMap[name] += number;
+    }
+
+    function getVotes(string memory name) public view returns (uint256){
+        return voteMap[name];
+    }
+
+    function resetVotes()  public {
+        for (uint256 i = 0; i < volunteers.length; i++) 
+        {
+            voteMap[volunteers[i]] = 0;
+        }
+    }
+}

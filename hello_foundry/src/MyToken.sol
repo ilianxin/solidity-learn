346d4a167704fcc32ade799817a288cd62af0ab1
Tata <ilianxin@126.com>
2025-10-03 20:45:40 +0800

    移除solmate子项目，更新MyToken合约构造函数以接受初始供应量，并将CounterB测试合约的Solidity版本更新至0.8.13

--- hello_foundry/src/MyToken.sol
+++ hello_foundry/src/MyToken.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(
-        
-    ) ERC20(name, symbol, decimals) {
-        _mint(msg.sender, 0);
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
     }
 }
-

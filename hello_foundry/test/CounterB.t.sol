fadc1137b2b9e91f4a816ce7d61050eaeb4e5521
Tata <ilianxin@126.com>
2025-10-03 20:26:11 +0800

    添加MyToken合约及CounterB测试合约，更新foundry.toml配置文件

---
+++ hello_foundry/test/CounterB.t.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+
+contract ContractBTest is Test {
+    uint256 testNumber;
+
+    function setUp() public {
+        testNumber = 42;
+    }
+
+    // 外部函数：用于通过 external 调用触发 underflow revert
+    function subtract43External() external {
+        testNumber -= 43;
+    }
+
+    function test_RevertWhen_Subtract43() public {
+        vm.expectRevert(stdError.arithmeticError);
+        this.subtract43External();
+    }
+
+    // function test_CannotSubtract43() public {
+    //     vm.expectRevert(stdError.arithmeticError);
+    //     console2.log("currentNumber= %d", testNumber);
+    //     testNumber -= 43;
+    // }
+
+}
